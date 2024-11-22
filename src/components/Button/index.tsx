b86a89cedb41e84319fd5ed8233081d477daa6c5
kauepsoares <kaue116@hotmail.com>
2024-11-22 16:59:41 -0300

    Iniciando projeto

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import style from './Button.module.scss'
+
+class Button extends React.Component <{ children: React.ReactNode }>{
+  render() {
+    return (
+      <button className={style.botao}>
+        {this.props.children}
+      </button>
+    )
+  }
+}
+
+export default Button;
---
+++ src/pages/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Form from '../components/Form';
+import List from '../components/List';
+import style from './App.module.scss'
+import Cronometer from '../components/Cronometer';
+
+function App() {
+  return (
+    <div className={style.AppStyle}>
+      <Form />
+      <List />
+      <Cronometer/>
+    </div>
+  );
+}
+
+export default App;
